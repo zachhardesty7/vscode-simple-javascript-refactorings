360796ca57f7477e56e8907ad5ddeb2bb6ed76e5
Zach Hardesty <hello@zachhardesty.com>
2021-08-15 19:50:34 -0500

    chore: add dirt simple test file

---
+++ test/results/singleLine.jsx
@@ -0,0 +1,9 @@
+/*
+eslint-disable
+  no-use-before-define,
+  @typescript-eslint/explicit-module-boundary-types,
+  react/jsx-curly-brace-presence,
+*/
+import * as React from "react"
+
+export const ReactComponent = () => <div property target="string" />
