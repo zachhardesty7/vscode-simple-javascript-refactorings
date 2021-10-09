6f8356bcbef1f61e75b591b71fd6db55f351a42c
Zach Hardesty <hello@zachhardesty.com>
2021-10-09 00:28:57 -0500

    chore: rm unnecessary eslint directives

--- test/inputs/jsxBraces/inlineLast.jsx
+++ test/inputs/jsxBraces/inlineLast.jsx
@@ -1,10 +1,3 @@
-/*
-eslint-disable
-  no-use-before-define,
-  @typescript-eslint/explicit-module-boundary-types,
-  react/jsx-curly-brace-presence,
-  import/no-extraneous-dependencies,
-*/
 import * as React from "react"
 
 export const ReactComponent = () => <div property target={"string"} />
